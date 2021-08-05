0d2236a1c9d468cfc6512cf7e0ef5dc71b85b870
Rishat-F <nilluziaf@gmail.com>
2021-08-05 19:22:24 +0300

    Added files for checking code with tox

--- main.py
+++ main.py
@@ -8,17 +8,23 @@ import jsonschema
 
 
 class InputParameterVerificationError(Exception):
+    """Error that raises when input parameter is not valid."""
+
     pass
 
 
 class ResultVerificationError(Exception):
+    """Error that raises when result of decorated function is not valid."""
+
     pass
 
 
-def decorator(input_validation: Callable[..., bool],
-              result_validation: Callable[[Any], bool],
-              on_fail_repeat_times: Optional[int] = 1,
-              default_behavior: Optional[Callable] = None) -> Callable:
+def decorator(
+    input_validation: Callable[..., bool],
+    result_validation: Callable[[Any], bool],
+    on_fail_repeat_times: int = 1,
+    default_behavior: Optional[Callable] = None,
+) -> Callable:
     """Multipurpose decorator with parameters.
 
     Checks if decorated function input paramaters and result are valid.
@@ -54,37 +60,37 @@ def decorator(input_validation: Callable[..., bool],
             If decorated function result is invalid and parameter
             default_behavior in decorator not defined.
     """
+
     def decoration(func: Callable) -> Callable:
         @wraps(func)
-        def wrapper(*args: Any, **kwargs: Any):
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
             errors_list = []
             if input_validation(*args, **kwargs):
                 result = func(*args, **kwargs)
                 if result_validation(result):
                     return result
                 else:
-                    errors_list.append(ResultVerificationError(
-                        f"Результат работы функции {result} невалиден!"))
+                    errors_list.append(ResultVerificationError(f"Результат работы функции {result} невалиден!"))
                     nonlocal on_fail_repeat_times
                     while on_fail_repeat_times != 0:
-                        print(f"Вызов функции после провала валидации "
-                              f"результата номер: {on_fail_repeat_times}")
+                        print(f"Вызов функции после провала валидации " f"результата номер: {on_fail_repeat_times}")
                         result = func(*args, **kwargs)
                         if result_validation(result):
                             return result
                         else:
-                            errors_list.append(ResultVerificationError(
-                                f"Результат работы функции {result} "
-                                f"невалиден!"))
+                            errors_list.append(
+                                ResultVerificationError(f"Результат работы функции {result} " f"невалиден!")
+                            )
                         on_fail_repeat_times -= 1
                     if default_behavior:
                         return default_behavior()
                     else:
                         raise errors_list[-1]
             else:
-                raise InputParameterVerificationError(
-                    f"Входной параметр {args, kwargs} невалиден!")
+                raise InputParameterVerificationError(f"Входной параметр {args, kwargs} невалиден!")
+
         return wrapper
+
     return decoration
 
 
@@ -93,22 +99,16 @@ SCHEMA = {
     "title": "Fake name creating schema",
     "description": "The root schema comprises the entire JSON document",
     "default": {},
-    "examples": [
-        {
-            "locale": "ru_RU"
-        }
-    ],
-    "required": [
-        "locale"
-    ],
+    "examples": [{"locale": "ru_RU"}],
+    "required": ["locale"],
     "additionalProperties": False,
     "properties": {
         "locale": {
             "description": "Defines the locale of name that should be created",
             "type": "string",
-            "enum": AVAILABLE_LOCALES
+            "enum": AVAILABLE_LOCALES,
         }
-    }
+    },
 }
 
 
@@ -129,18 +129,16 @@ def validate_locale(argument: dict) -> bool:
 
 def check_symbols(result: str) -> bool:
     """Check if string including only latin symbols and space."""
-    regex = re.compile(rf"^[a-zA-Z ]+$")
+    regex = re.compile(r"^[a-zA-Z ]+$")
     return bool(regex.match(result))
 
 
-def default_behavior_for_create_fake_name():
-    """Some function that should be called if result of
-    create_fake_name function is invalid."""
-    print("Function didn't create a name without "
-          "non english symbols and '-' symbol")
+def default_behavior_for_create_fake_name() -> None:
+    """Function that should be called if result of create_fake_name function is invalid."""
+    print("Function didn't create a name without non english symbols and '-' symbol")
 
 
-@decorator(validate_locale, check_symbols, on_fail_repeat_times=1)
+@decorator(validate_locale, check_symbols, on_fail_repeat_times=0)
 def create_fake_name(argument: dict) -> str:
     """Create fake name.
 
